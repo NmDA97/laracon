19d29cacac53926b018d3fe434389e1d1b447a9f
nadun <nadhun97@gmail.com>
2024-02-19 00:58:09 +0530

    a

---
+++ app/Models/Venue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Region;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Venue extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'id' => 'integer',
+        'region' => Region::class,
+    ];
+
+    public function conferences(): HasMany
+    {
+        return $this->hasMany(Conference::class);
+    }
+}
